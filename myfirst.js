0ea03ca1b95b5ed9b090bf9992e3bb83f3c007b5
Rando Talviste <43164725+randtal@users.noreply.github.com>
2019-07-03 17:23:47 +0300

    Add files via upload

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,9 @@
-//require module
+//require module(s)
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   //Prints to the window
-  res.end('Hello World!');
+  res.end('Hello world!');
   //listen to this port
 }).listen(8080);
